685c207677b6030019105af037b1d64eb13200bc
baruch <baruch@ibn-labs.com>
2022-09-30 17:10:46 +0300

    initial

---
+++ sensors.py
@@ -0,0 +1,37 @@
+import serial
+import json 
+
+class Weight:
+    w = 0
+    samples =0
+
+    def add_sample(self, s):
+        w = s
+
+class Acceleration:
+    a = [0,0,0]
+    samples =0
+    
+    def add_sample(self, s):
+        w = s
+
+def listener():
+    ser = serial.Serial('/dev/ttyUSB0', 115200)  # open serial port
+    print(ser.name)         # check which port was really used
+    ser.write(b'hello')     # write a string
+
+    while 1:
+        line = ser.readline()
+        try:
+            j = json.loads("{"+line.decode("utf-8")+"}")
+            process_line(j)
+        print(line)
+
+    ser.close()             # close port
+
+#{'HX711_0': {'data': 16659756}, 'HX711_1': {'data': 137991}, 'MPU0': {'ax': -6204, 'ay': -14972, 'az': -1872, 'temp': 1648}, 'MPU1': {'ax': 16228, 'ay': 2912, 'az': -6084, 'temp': 2832}}
+def process_line(j):
+
+
+if __name__ == "__main__":
+    listener()
