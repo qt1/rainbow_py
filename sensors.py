d8cbf32adc1e2e7a7eee65bff80ab680dd67694a
Guy Levitsky <guy.l@datarails.com>
2022-10-02 20:52:26 +0300

    correct assignment and exception handling

--- sensors.py
+++ sensors.py
@@ -1,19 +1,25 @@
+import logging
+
 import serial
 import json 
 
+
 class Weight:
     w = 0
-    samples =0
+    samples = 0
 
     def add_sample(self, s):
-        w = s
+        self.w = s
+        self.samples += 1
+
 
 class Acceleration:
-    a = [0,0,0]
-    samples =0
+    a = [0, 0, 0]
+    samples = 0
     
     def add_sample(self, s):
-        w = s
+        self.a = s
+        self.samples += 1
 
 def listener():
     ser = serial.Serial('/dev/ttyUSB0', 115200)  # open serial port
@@ -25,12 +31,15 @@ def listener():
         try:
             j = json.loads("{"+line.decode("utf-8")+"}")
             process_line(j)
+        except Exception as e:
+            logging.exception("Exception while reading data from sensors!")
         print(line)
 
     ser.close()             # close port
 
 #{'HX711_0': {'data': 16659756}, 'HX711_1': {'data': 137991}, 'MPU0': {'ax': -6204, 'ay': -14972, 'az': -1872, 'temp': 1648}, 'MPU1': {'ax': 16228, 'ay': 2912, 'az': -6084, 'temp': 2832}}
 def process_line(j):
+    print(j)
 
 
 if __name__ == "__main__":
