e599ed13ce0cf0713c8248466f7c231dcbb35c76
Guy Levitsky <guy.l@datarails.com>
2022-10-07 09:06:11 +0300

    close file before running animation

--- rainbow_main.py
+++ rainbow_main.py
@@ -20,10 +20,11 @@ if __name__ == '__main__':
     for i, music_file_path in enumerate(playlist):
         out_file_name = animations[i % len(animations)]
         try:
+            bytes_data = None
             with open(out_file_name, mode='rb') as file:  # b is important -> binary
                 bytes_data = file.read()
-                led_player = LedPlayer(sequence_type=SequenceTypeChoices.FRAME, bytes_data=bytes_data, music_file_path=music_file_path, sensors=sensors)
-                led_player.play_animation()
+            led_player = LedPlayer(sequence_type=SequenceTypeChoices.FRAME, bytes_data=bytes_data, music_file_path=music_file_path, sensors=sensors)
+            led_player.play_animation()
         except Exception as e:
             LedPlayer.stop_music()
             if led_player:
